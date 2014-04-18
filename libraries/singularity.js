9e47c3a791ed4f9f7936f3f4b33351d011e18597
chr0n1x <heilong24@gmail.com>
2014-04-18 16:03:31 -0400

    libraries/singularity: updating loadListener() to use app as the context var instead of self

--- libraries/singularity.js
+++ libraries/singularity.js
@@ -5,22 +5,24 @@ var Logger = require('./log'),
 
 module.exports = function(config) {
 
-  var loadListeners = function(directories) {
+  var app = express();
+  app.log = Logger(config.log_level || 'debug');
+  app.listeners = [];
+
+  app.loadListeners = function(directories) {
     if (!Array.isArray(directories)) {
       throw new Error('directories must be array');
     }
 
-    var self = this;
-
     directories.forEach(function(dir) {
       fs.readdir(dir, function(err, files) {
         if (err) {
-          self.log.error(err);
+          app.log.error(err);
           process.exit(1);
         }
 
         if (!config.plugins) {
-          self.log.info('No plugin configurations found');
+          app.log.info('No plugin configurations found');
           process.exit(0);
         }
 
@@ -33,29 +35,23 @@ module.exports = function(config) {
           }
 
           if (!config.plugins[pluginName]) {
-            self.log.info('No configuration for ' + pluginName + ', not loading');
+            app.log.info('No configuration for ' + pluginName + ', not loading');
             continue;
           }
 
           var pluginEnabled = config.plugins[pluginName].enabled;
 
           if (pluginEnabled == undefined || pluginEnabled) {
-            self.log.info('Loading plugin: ' + pluginName);
-            self.listeners.push(require(filename).init(config.plugins[pluginName], app));
+            app.log.info('Loading plugin: ' + pluginName);
+            app.listeners.push(require(filename).init(config.plugins[pluginName], app));
           }
           else {
-            self.log.info('Not loading disabled plugin ' + pluginName);
+            app.log.info('Not loading disabled plugin ' + pluginName);
           }
         }
       });
     });
   };
-
-  var app = express();
-
-  app.log = Logger(config.log_level || 'debug');
-  app.listeners = [];
-  app.loadListeners = loadListeners;
 
   return app;
 };
