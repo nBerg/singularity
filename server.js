28966e5f5557a74d19850c8469b15ff62615d8c5
chr0n1x <heilong24@gmail.com>
2014-05-30 18:21:27 -0400

    Adding comment endpoint

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.post('/', function(request, response) {
 app.use('/pull_requests', require('./routes/pull_requests').init(app));
 app.use('/merge', require('./routes/merge').init(app));
 app.use('/config', require('./routes/cfg').init(app));
+app.use('/comment', require('./routes/comment').init(app));
 
 var server = app.listen(config.port || 80, function() {
   app.log.info('Listening on port %d', server.address().port);
