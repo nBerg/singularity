23a641bdac015a9d07c249551aebf8d18ed49843
Nina Berg <nikb100@gmail.com>
2014-06-16 14:23:48 -0400

    Fix vent setChannel

--- libraries/vent.js
+++ libraries/vent.js
@@ -1,25 +1,28 @@
 "use strict";
 
-var postal = require('postal'),
-channel;
+var postal = require('postal');
 
 // vent: something turns a bunch of knobs
 // ...and things just keep coming out
 // ...of all the...holes...? :|
 module.exports = require('nbd/Class').extend({
   name: 'unnamed-vent',
+  channel: undefined,
 
   setChannel: function(channelName) {
-    channel = postal.channel(channelName);
+    this.channel = postal.channel(channelName);
   },
 
   publish: function(topic, data) {
-    this.log.debug(this.name + ': ' + topic);
-    if (!channel) {
+    this.log.debug(this.name + ' vent publishing on channel:');
+    this.log.debug(this.channel.channel);
+    this.log.debug('with topic: ' + topic);
+
+    if (!this.channel) {
       this.error('cannot publish topic, no channel', {topic: topic, vent: this.name});
       return;
     }
-    channel.publish(topic, data);
+    this.channel.publish(topic, data);
   },
 
   init: function(option) {
