11834a4e2d62748791b92d66c39ca884caaef9c5
chr0n1x <heilong24@gmail.com>
2014-06-17 00:15:40 -0400

    build adapter: starting to split out into its own abstraction layer

--- libraries/adapters/build.js
+++ libraries/adapters/build.js
@@ -1,24 +1,63 @@
 "use strict";
 
 var q = require('q'),
-    async = require('async');
+fs = require('fs'),
+path = require('path'),
+async = require('async'),
+clientConnection;
+
+function getClient() {
+  if (clientConnection) {
+    return clientConnection;
+  }
+  throw 'No DB client - error connecting on startup? Incorrect configuration?';
+}
 
 module.exports = require('../vent').extend({
   init: function(option) {
     this._super(option);
   },
 
-  start: function() {
-    var self = this,
-    timeout = this.config.frequency || 2000;
+  // todo: DRY b/w here & adapters/db
+  setClient: function(client) {
+    if (!this.config.get(client)) {
+      this.log.error('No config for db.<client>, ignoring', {client: client});
+      return;
+    }
 
+    var clientPath = this.config.get('client_path') ||
+                     path.join(__dirname, '../builders/dbs/', client + '.js');
+    if (fs.existsSync(clientPath)) {
+      var ClientObj = require(clientPath);
+
+      q.fcall(function() {
+        return new ClientObj(this.config.get(client));
+      }.bind(this))
+      .then(function(instance) {
+        clientConnection = instance;
+      })
+      .catch(this.log.error)
+      .done();
+
+      return;
+    }
+
+    this.log.error(clientPath + ' does not exist, ignoring');
+  },
+
+  start: function() {
+    if (this.config.method === 'hooks') {
+      this.publish('build.polling');
+      return;
+    }
+
+    var self = this;
     async.parallel({
       poll: function() {
         var pingForPoll = function() {
-          self.publish('jobs.polling');
-          setTimeout(pingForPoll, timeout);
+          self.publish('build.polling');
+          setTimeout(pingForPoll, self.config.frequency || 2000);
         };
-
         pingForPoll();
       }
     });
@@ -30,5 +69,19 @@ module.exports = require('../vent').extend({
 
   checkPushJob: function(push) {
 
+  },
+
+  triggerBuild: function(req_body) {
+    var self = this;
+
+    return q.fcall(getClient)
+    .thenResolve(req_body)
+    .then(validateOpts)
+    .then(clientConnection.triggerBuild)
+    .then(function(res) {
+      self.publish('build.triggered', req_body);
+    })
+    .catch(self.error)
+    .done();
   }
 });
