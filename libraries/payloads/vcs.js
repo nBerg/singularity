9237003a3e142ce17caa6379285de019185854a0
chr0n1x <heilong24@gmail.com>
2014-06-24 10:33:48 -0400

    payloads/vcs: more-or-less decided on schema

---
+++ libraries/payloads/vcs.js
@@ -0,0 +1,24 @@
+"use strict";
+
+module.exports.VcsPayload = require('./payload').extend({
+  payload: 'vcs',
+  required_fields: [
+      'repo',
+      'before',
+      'after',
+      'actor',    // who dunnit
+
+      'repo_url',
+      'base_ref', // base branch
+      'fork_url',
+      'fork_ref', // fork branch
+
+      'status',
+      'repo_id',  // github ID of repo
+      'change',   // pull_request number
+      'change_id' // actual pull_request ID
+  ],
+  field_vals: {
+    status: ['open', 'closed', 'merged']
+  }
+});
