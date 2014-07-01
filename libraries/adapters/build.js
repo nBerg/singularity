f873598466d00d60f2a7772fed93ed3eba90ab87
chr0n1x <heilong24@gmail.com>
2014-07-01 01:22:43 -0400

    adapters/build: first poke at the build adapter

--- libraries/adapters/build.js
+++ libraries/adapters/build.js
@@ -1,22 +1,45 @@
 "use strict";
 
-var q = require('q'),
-async = require('async');
+var q = require('q');
 
-function validateOpts(opts) {
-  return opts;
+function buildChangePayload(vcsPayload) {
+  return q(vcsPayload)
+  .then(validateChange.bind(this))
+  .thenResolve(vcsPayload)
+  .then(buildChange.bind(this));
+}
+
+function buildProposalPayload(vcsPayload) {
+  return q(vcsPayload)
+  .then(validateProposal.bind(this))
+  .thenResolve(vcsPayload)
+  .then(buildProposal.bind(this));
+}
+
+function executeAndPublish(workflow, payload) {
+  this.debug(
+    'request for build received',
+     this.logForObject(payload)
+  );
+  this.executeInPlugins(workflow, payload)
+  .then(function(pluginResults) {
+    pluginResults.forEach(function(buildResults) {
+      buildResults.forEach(function(buildPayload) {
+        this.publishPayload(buildPayload);
+      });
+    }.bind(this));
+  }.bind(this));
 }
 
 module.exports = require('./adapter').extend({
-  init: function(option) {
-    this.name = 'build';
-    this.pluginType = 'builders';
-    this._super(option);
-  },
+  name: 'build',
+  pluginType: 'builders',
 
   buildChange: function(changePayload) {
+    executeAndPublish.call(this, buildChangePayload, changePayload);
   },
 
   buildProposal: function(proposalPayload) {
+    executeAndPublish.call(this, buildProposalPayload, proposalPayload);
   }
 });
